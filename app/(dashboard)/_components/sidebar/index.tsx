d36d4837327f8a5ceb150d77cff9ff1b8c9c4f48
IvanDanyliuk <ivandaniliuk@gmail.com>
2024-08-29 19:49:28 +0300

    create a basic layout of the Templates component

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import Link from 'next/link';
-import { Clock, Star } from 'lucide-react';
 import { usePathname } from 'next/navigation';
+import { Clock, Star } from 'lucide-react';
+import { Templates } from '../templates';
 
 
 export const Sidebar = () => {
@@ -10,20 +11,20 @@ export const Sidebar = () => {
   
   return (
     <aside className='fixed h-full flex'>
-      <div className='w-[50px] h-full py-3 bg-secondary'>
+      <div className='w-[50px] h-full py-3 bg-secondary-bg'>
         Team List
       </div>
-      <div className='w-[170px] px-2 py-3 flex flex-col gap-y-3 bg-main-bg'>
-        <Link href='/' className={`flex items-center gap-1 ${pathname === '/' ? 'text-primary' : 'text-black'}`}>
+      <div className='w-[180px] px-2 py-3 flex flex-col gap-y-3 bg-page-bg'>
+        <Link href='/' className={`flex items-center gap-1 ${pathname === '/' ? 'text-primary-bg' : 'text-black'}`}>
           <Clock className='w-5 h-5' />
           <span>Recent boards</span>
         </Link>
-        <Link href='/starred-boards' className={`flex items-center gap-1 ${pathname === '/starred-boards' ? 'text-primary' : 'text-black'}`}>
+        <Link href='/starred-boards' className={`flex items-center gap-1 ${pathname === '/starred-boards' ? 'text-primary-bg' : 'text-black'}`}>
           <Star className='w-5 h-5' />
           <span>Starred Boards</span>
         </Link>
         <div>Team Dropdown Menu</div>
-        <div>Templates</div>
+        <Templates />
       </div>
     </aside>
   );
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -9,8 +9,11 @@ const DashboardLayout = ({ children }: IDashboardLayout) => {
   return (
     <main className='h-full'>
       <Sidebar />
-      <div className='pl-[220px] flex-1 h-screen bg-main-bg'>
-        {children}
+      <div className='pl-[230px] flex-1 h-screen bg-page-bg'>
+        <div className='h-[80px] bg-amber-500'>Search</div>
+        <div className='h-[calc(100%-80px)] bg-cyan-500'>
+          {children}
+        </div>
       </div>
     </main>
   );
