e90b85007eb7405d658a8bde534169574c6cb6cb
IvanDanyliuk <ivandaniliuk@gmail.com>
2024-09-02 21:31:20 +0300

    add the auth setup and create the AuthProvider

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,10 @@
 import { type NextRequest } from 'next/server'
-import { updateSession } from './lib/supabase/middleware'
-
+import { updateSession } from './lib/db/clients/middleware'
+// import { updateSession } from '@/utils/supabase/middleware'
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+  // update user's auth session
+  return await updateSession(request);
 }
 
 export const config = {
