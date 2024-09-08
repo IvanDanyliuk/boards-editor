1a0e5643dfeef746d7f98fa3fba7cb50ef16b332
IvanDanyliuk <ivandaniliuk@gmail.com>
2024-09-08 15:31:43 +0300

    create the basic layout of the Searchbar component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,6 +2,7 @@ import { redirect } from 'next/navigation';
 import { getCurrentUser } from '@/lib/actions/auth.actions';
 import { Sidebar } from './_components/sidebar';
 import { UserMenu } from './_components/user-menu';
+import { Searchbar } from './_components/search-bar';
 
 
 interface IDashboardLayout  {
@@ -21,7 +22,7 @@ const DashboardLayout = async ({ children }: IDashboardLayout) => {
       <Sidebar />
       <div className='pl-[230px] flex-1 h-screen bg-page-bg'>
         <div className='px-3 h-[80px] flex justify-between items-center bg-amber-500'>
-          <div>Search</div>
+          <Searchbar />
           <UserMenu user={data.user} error={error} />
         </div>
         <div className='h-[calc(100%-80px)] bg-cyan-500'>
