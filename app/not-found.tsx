4eca15f40b0c5f348e8bab6f505719ca73278bf0
IvanDanyliuk <ivandaniliuk@gmail.com>
2024-10-08 13:51:47 +0300

    code styling

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
-import { ArrowLeft } from 'lucide-react';
 import Image from 'next/image';
+import { ArrowLeft } from 'lucide-react';
 
 
 const NotFoundPage = () => {
@@ -8,11 +8,24 @@ const NotFoundPage = () => {
     <div className='w-full h-screen flex flex-col justify-center items-center gap-6'>
       <div className='w-full flex flex-col justify-center items-center gap-1'>
         <p className='text-6xl text-gray-300 font-extrabold'>404</p>
-        <Image src='/images/not-found-placeholder.jpg' alt='image' width={400} height={400} className='my-3' />
-        <p className='text-2xl text-gray-400 font-bold'>Page not found</p>
-        <p className='text-lg text-gray-400 font-normal'>Sorry, the page you are looking for cannot be found</p>
+        <Image 
+          src='/images/not-found-placeholder.jpg' 
+          alt='image' 
+          width={400} 
+          height={400} 
+          className='my-3' 
+        />
+        <p className='text-2xl text-gray-400 font-bold'>
+          Page not found
+        </p>
+        <p className='text-lg text-gray-400 font-normal'>
+          Sorry, the page you are looking for cannot be found
+        </p>
       </div>
-      <Link href='/' className='flex items-center'>
+      <Link 
+        href='/' 
+        className='flex items-center'
+      >
         <ArrowLeft className='mr-1' />
         Go back to the home page
       </Link>
